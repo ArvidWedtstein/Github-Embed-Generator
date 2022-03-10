f1ab3632d4a91d278297ba88d78df3d4c46bbff4
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-03-10 16:35:38 +0100

    d

--- main.go
+++ main.go
@@ -121,7 +121,7 @@ func getSkills(c *gin.Context) {
 
 func resonance(c *gin.Context) {
 	c.Header("Content-Type", "image/svg+xml")
-	c.String(http.StatusAccepted, `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500" onload="anim()">
+	c.String(http.StatusOK, `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500" onload="anim()">
 	<style>
 	
 	#lineGroup line {
